67b5108acaa913c4d2c3c658399e08c4e55419a4
WakaCappie <omniknight13@mail.ru>
2022-07-06 14:00:30 +0300

    routersReady

--- cian.js
+++ cian.js
@@ -11,15 +11,15 @@ config(app);
 const homeRouter = require('./routes/homeRouter');
 const userRouter = require('./routes/userRouter');
 const tagRouter = require('./routes/tagRouter');
-// const entriesRouter = require('./routes/entries.routes');
-// const authRuter = require('./routes/auth.routes');
+const entriesRouter = require('./routes/entriesRouter');
+const authRouter = require('./routes/authRouter');
 
 const PORT = process.env.PORT ?? 3000;
 
 app.use('/', homeRouter);
 app.use('/user', userRouter);
 app.use('/tags', tagRouter);
-// app.use('/entries', entriesRouter);
-// app.use('/auth', authRuter);
+app.use('/entries', entriesRouter);
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => console.log(`<===== SERVER START at ${PORT} =====>`));
