9cfa302f9160675de782f1ec45b3a82a5fe1eb52
kadjik <kadjik_gasparyan@mail.ru>
2022-07-06 15:47:14 +0300

    release 0

---
+++ db/models/favorite.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Favorite extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Houing }) {
+      Favorite.belongsTo(User, { foreignKey: '{ user_id }' });
+      Favorite.belongsTo(Houing, { foreignKey: '{ housing_id }' });
+      // define association here
+    }
+  }
+  Favorite.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+    housing_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Housings',
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Favorite',
+  });
+  return Favorite;
+};
